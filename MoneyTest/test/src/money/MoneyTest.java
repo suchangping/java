0d9050c94b706f859fb0c8f45e668952cf2c2d06
02200640 <zhexue2002@gamil.com>
2021-05-18 23:36:30 +0900

    第一章　$5*2  まず作り　クラス後で追加

---
+++ MoneyTest/test/src/money/MoneyTest.java
@@ -0,0 +1,11 @@
+package money;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five =new Dollar(5);  //five インスタンス生成　
+        five.times(2);
+        assertEquals(10,five.amount);
+    }
+}
