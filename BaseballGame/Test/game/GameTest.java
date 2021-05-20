e6cdcbba9e23074afaece58ea94b62be366608fe
02200640 <zhexue2002@gamil.com>
2021-05-20 15:49:42 +0900

    野球ゲーム修正

--- BaseballGame/Test/game/GameTest.java
+++ BaseballGame/Test/game/GameTest.java
@@ -1,5 +1,23 @@
 package game;
 
-public class GameTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class GameTest {
+    @Test
+    public void testScores() {
+        Team teamA = new Team();
+        teamA.setName("ソフトバンク");
+        Team teamB = new Team();
+        teamB.setName("楽天");
+
+        teamA.scores = new int[]{1, 2, 0, 1, 0, 0, 0, 0};
+        teamB.scores = new int[]{1, 2, 0, 1, 0, 0, 0, 6};
+        assertEquals(4, teamA.getSumScore());
+
+    }
+    @Test
+    public void testRuner() {
+
+    }
 }
